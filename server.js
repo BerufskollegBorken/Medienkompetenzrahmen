dde094c719429858660f63674ab0da05556aa3c9
stbaeumer <baeumer@posteo.de>
2020-03-14 07:26:57 +0100

    ...

---
+++ server.js
@@ -0,0 +1,79 @@
+const iban = require('iban')
+const express = require('express')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const mysql = require('mysql')
+const validator = require("email-validator");
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
+app.use(cookieParser())
+
+const server = app.listen(process.env.PORT || 3000, () => {
+    console.log('Server lauscht auf Port %s', server.address().port)    
+})
+
+class Bildungsgangfach{
+    constructor(){
+        this.Hardware        
+    }
+}
+
+app.get('/',(req, res, next) => {   
+    res.render('index.ejs', {                              
+    })
+})
+
+var env = process.env.NODE_ENV || 'development';
+var config = require('./config')[env];
+
+const dbVerbindung = mysql.createConnection({
+    host: config.database.host,
+    user: config.database.user,
+    password: config.database.password,
+    database: config.database.db
+})
+
+dbVerbindung.connect(function(fehler){
+    dbVerbindung.query('CREATE MKR kunde(fach VARCHAR(45), bildungsgang VARCHAR(45), hardwareausstattung, PRIMARY KEY(fach,bildungsgang));', function (fehler) {
+        if (fehler) {
+            if(fehler.code == "ER_TABLE_EXISTS_ERROR"){
+                console.log("Tabelle MKR existiert bereits und wird nicht angelegt.")
+            }else{
+                console.log("Fehler: " + fehler )
+            }
+        }else{
+            console.log("Tabelle MKR erfolgreich angelegt.")
+        }
+    })
+})
+
+app.get('/login',(req, res, next) => {         
+    res.cookie('istAngemeldetAls', '')       
+    res.render('login.ejs', {                    
+    })
+})
+
+app.post('/',(req, res, next) => {       
+    
+    var änderung = false;
+    
+    if(req.body.hardwareausstattung != bildungsgangfach.hardwareausstattung){
+      bildungsgangfach.Hardwareausstattung = req.body.hardwareausstattung  
+      änderung = true
+      console.log("Änderung: Hardware: " + req.body.hardwareausstattung )
+    }
+
+    if(änderung){        
+        if(req.cookies['istAutorisiert']){
+            //todo: writetodb
+        }else{
+            res.render('login.ejs', {                    
+            })
+        }        
+    }else{            
+        console.log("Es ligt keine Änderung an.")       
+    }
+})
+
